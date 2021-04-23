ddb94d86f1242fd566c2a8fb845d8844c64493a2
Amine32 <aminov.02@hotmail.com>
2021-04-23 04:05:02 +0300

    Added Correct App Layout

--- app/src/main/java/com/example/imageapp/SettingActivity.kt
+++ app/src/main/java/com/example/imageapp/SettingActivity.kt
@@ -2,17 +2,10 @@ package com.example.imageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.imageapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        CameraButton.setOnClickListener{
-
-        }
-
+        setContentView(R.layout.activity_setting)
     }
 }
