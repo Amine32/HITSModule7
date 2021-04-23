72279ea01cba1bcd2ab9947e4f6706002b4aa387
Amine32 <aminov.02@hotmail.com>
2021-04-23 23:07:16 +0300

    Added Image Functionality (app crashes)

--- app/src/main/java/com/example/imageapp/MainActivity.kt
+++ app/src/main/java/com/example/imageapp/MainActivity.kt
@@ -1,18 +1,86 @@
 package com.example.imageapp
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.imageapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CameraButton.setOnClickListener{
+        CameraButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //permission not enabled
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
+                    requestPermissions(permission, PERMISSION_CODE)
+
+                } else {
+                    //permission already granted
+                    openCamera()
+                }
+            }
+            else {
+                //system os is < marshmallow
+                openCamera()
+            }
         }
 
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+        when(requestCode) {
+            PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted
+                    openCamera()
+                }
+                else {
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if(resultCode == Activity.RESULT_OK) {
+            //set image captured to image view
+            imageView.setImageURI(image_uri)
+            //val intent = Intent(this, SettingActivity::class.java)
+            //startActivity(intent)
+        }
     }
 }
