f0a941cb137ed821650806439cb92d74cd8b4deb
yahya-abdul-majeed <yahya.zf2@gmail.com>
2021-04-29 03:49:14 +0500

    Added Gallery button, but camera doesn't work now

--- app/src/main/java/com/example/imageapp/MainActivity.kt
+++ app/src/main/java/com/example/imageapp/MainActivity.kt
@@ -10,8 +10,11 @@ import android.app.Activity
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Camera
 import android.net.Uri
 import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_setting.*
 
@@ -20,10 +23,15 @@ class MainActivity : AppCompatActivity() {
     private val PERMISSION_CODE = 1000
     private val IMAGE_CAPTURE_CODE = 1001
     var image_uri: Uri? = null
+    companion object{
+        private val IMAGE_PICK_CODE=1002
+        private val PERMISSION_CODE_GALLERY=1003
 
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         CameraButton.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -45,10 +53,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         GalleryButton.setOnClickListener {
-            val intent = Intent(this@MainActivity, SettingActivity::class.java)
-            startActivity(intent)
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+                    val permissions= arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE_GALLERY)
+                }else{
+                    pickImageFromGallery();
+                }
+            }else{
+                pickImageFromGallery();
+            }
         }
 
+    }
+
+    private fun pickImageFromGallery() {
+        val requestCode = 0
+        val launchGalleryIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(launchGalleryIntent, requestCode)
+        //val intent= Intent(Intent.ACTION_PICK)
+        //intent.type="image/*"
+        //startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
     private fun openCamera() {
@@ -75,17 +101,50 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
-        }
+            PERMISSION_CODE_GALLERY->{
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted
+                    pickImageFromGallery()
+                }
+                else {
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+
+            }            }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         //called when image was captured from camera intent
-        if(resultCode == Activity.RESULT_OK) {
+        if(resultCode == Activity.RESULT_OK && requestCode==IMAGE_CAPTURE_CODE) {
             //set image captured to image view
             val intent = Intent(this@MainActivity, SettingActivity::class.java)
             intent.putExtra("img", image_uri.toString())
             startActivity(intent)
         }
+        if (requestCode === 0 && resultCode === Activity.RESULT_OK ) {
+            val selectedImage: Uri? = data?.data
+            val intent = Intent(this, SettingActivity::class.java)
+            intent.putExtra("image_from_gallery",
+                selectedImage.toString())
+            startActivity(intent)
+        }
+    }
+    fun getRealPathFromURI(contentURI: Uri?, context: Activity): String? {
+        val projection =
+            arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = context.managedQuery(
+            contentURI, projection, null,
+            null, null
+        )
+            ?: return null
+        val column_index = cursor
+            .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        return if (cursor.moveToFirst()) {
+            // cursor.close();
+            cursor.getString(column_index)
+        } else null
+        // cursor.close();
     }
 }
--- app/src/main/java/com/example/imageapp/SettingActivity.kt
+++ app/src/main/java/com/example/imageapp/SettingActivity.kt
@@ -1,17 +1,41 @@
 package com.example.imageapp
 
+import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_setting.*
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_setting.*
+import java.io.InputStream
+
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        var img = Uri.parse(intent.getStringExtra("img"))
-        imageView.setImageURI(img)
+
+
+
+        val resId = intent.getStringExtra("image_from_gallery")
+        //val imgThumbnail: ImageView = findViewById(R.id.imageView)
+
+        val imageStream: InputStream? = contentResolver.openInputStream(Uri.parse(resId))
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+
+        imageView.setImageBitmap(bitmap)
+
+       // var img = Uri.parse(intent.getStringExtra("img"))
+        //imageView.setImageURI(img)
+
+
+
+
+
+
+
     }
 }
+
+
